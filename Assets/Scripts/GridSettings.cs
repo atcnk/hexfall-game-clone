329e26239d70155999a5971cf62b371a90c963eb
Atacan Kilic <uaatacan@gmail.com>
2022-02-11 03:04:02 +0300

    +refactor: Gridsettings - new color data

--- Assets/Scripts/GridSettings.cs
+++ Assets/Scripts/GridSettings.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Hexfall.Grid
 {
@@ -8,8 +9,10 @@ namespace Hexfall.Grid
     {
         [SerializeField] private int _width;
         [SerializeField] private int _height;
+        [SerializeField] private List<Color> _hexesColors = new List<Color>();
 
         public int Width { get => _width; }
-        public int Height { get => _height; }   
+        public int Height { get => _height; }
+        public List<Color> HexesColors { get => _hexesColors; }
     }
 }
